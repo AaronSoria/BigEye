4702228d040042d8ec2971345bf105f9e2866469
Aaron Soria <soria_aaron@outlook.com>
2020-06-20 21:21:54 -0300

    acciones de los botones realizada, falta vincular accion de analisis

--- BigEyeInitView/BigEyeInitViewModel.py
+++ BigEyeInitView/BigEyeInitViewModel.py
@@ -1,6 +1,10 @@
 from BigEyeInit_ui import *
+from PyQt5.QtWidgets import *
 
 class BigEyeInitView(QtWidgets.QMainWindow, Ui_MainWindow):
+    currentFolderName = ""
+    currentFileName = ""
+     
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
@@ -13,10 +17,20 @@ class BigEyeInitView(QtWidgets.QMainWindow, Ui_MainWindow):
         pass
 
     def VideoSearch(self):
-        pass
+        options = QFileDialog.Options()
+        options = QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"Buscar Video", "","All mp4 Files (*.mp4)", options=options)
+        if fileName:
+            self.set_currentFileName(fileName)
+            self.entryVideoInput.setPlainText(fileName)
 
     def SetResultDestiny(self):
-        pass
+        options = QFileDialog.Options()
+        options = QFileDialog.DontUseNativeDialog
+        folderName = QFileDialog.getExistingDirectory(self, "Seleccione carpeta de destino")
+        if folderName:
+            self.set_currentFolderName(folderName)
+            self.entryImageOutput.setPlainText(folderName)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
--- FindSuspects.py
+++ FindSuspects.py
@@ -3,49 +3,49 @@ import cv2
 import datetime
 import matplotlib.pyplot as plt
 
-def detectar(imagen):
+def searchPerson(imagen):
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector( cv2.HOGDescriptor_getDefaultPeopleDetector() )
     # Capturo los puntos en donde posiblemente se encuentre la persona
     rects, weights = hog.detectMultiScale(imagen,winStride=(8, 8),padding=(16, 16),scale=1.05)
+    return len(rects) != 0
 
-    # Dibujo rectangulo
-    for i, (x, y, w, h) in enumerate(rects):
-        cv2.rectangle(imagen,(x,y),(x+w,y+h),(0,255,0),10)  
-        fuente=cv2.FONT_HERSHEY_SIMPLEX
-        peso=weights[i]
-        y2 = x - 15 if x - 15 > 15 else x + 15
-        cv2.putText(imagen, str(peso), (h, y2), fuente, 0.75, (0, 255, 0),2)
-    return imagen
 
 # Empiezo a capturar el tiempo
 myTime =  datetime.datetime.now()
 
-video= cv2.VideoCapture('/home/stark28/Documentos/tecnica3.mp4')
+video= cv2.VideoCapture('D:\\Proyectos\\BigEye\\tecnica3.mp4')
 
 fps=video.get(cv2.CAP_PROP_FPS)
 codec =cv2.VideoWriter_fourcc(*'XVID')
+
+timestamps = [video.get(cv2.CAP_PROP_POS_MSEC)]
+calc_timestamps = [0.0]
+
 size = (int(video.get(cv2.CAP_PROP_FRAME_WIDTH)),
         int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-nuevoVideo= cv2.VideoWriter('/home/stark28/python_projects/Big_Eye/resultados/something.mp4',codec,fps,tuple(it//2 for it in size))  
 
 i=0
 success, frame = video.read()
 while success:
     i+=1
     #frame=cv2.resize(frame,(1280, 720))
+    isPerson = False
     rgb= cv2.cvtColor(frame[::2,::2], cv2.COLOR_BGR2RGB)
+    print("el frame es: " + str(i))
     if (i%2 == 0):
-        nuevo_rgb= detectar(rgb)
-    else:
-        nuevo_rgb=rgb
-    print(str(i))
-    nuevo_bgr= cv2.cvtColor(nuevo_rgb, cv2.COLOR_RGB2BGR)
-    nuevoVideo.write(nuevo_bgr)
-    success, frame = video.read()    
-video.release()
-nuevoVideo.release()
+        isPerson = searchPerson(rgb)
+    success, frame = video.read()
 
+    if isPerson:
+        if success:
+            timestamps.append(video.get(cv2.CAP_PROP_POS_MSEC))
+            calc_timestamps.append(calc_timestamps[-1] + 1000/fps)
+    
+video.release()
 print("")
 print("[INFO] Listo")
 print("[INFO] tiempo: {}s".format((datetime.datetime.now() - myTime).total_seconds()))
+
+for i, (ts, cts) in enumerate(zip(timestamps, calc_timestamps)):
+    print('[WARNING] hay una persona en: ',str(abs(ts - cts)))
