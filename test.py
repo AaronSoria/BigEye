944a38fd9d87d2f2e0f2e30ae8b62b6feb607175
J.A. Rodrigo Puca <r@geeks.ms>
2020-07-05 18:51:21 -0300

    recursos de prueba

--- test.py
+++ test.py
@@ -4,17 +4,18 @@ import datetime
 import matplotlib.pyplot as plt
 
 
-video= cv2.VideoCapture('/home/stark28/Documentos/tecnica3.mp4')
+video= cv2.VideoCapture('./res/video.mp4')
 
 fps=video.get(cv2.CAP_PROP_FPS)
 codec =cv2.VideoWriter_fourcc(*'XVID')
 size = (int(video.get(cv2.CAP_PROP_FRAME_WIDTH)),
         int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-nuevoVideo= cv2.VideoWriter('/home/stark28/python_projects/Big_Eye/resultados/something.mp4',codec,fps,tuple(it//2 for it in size))  
+# nuevoVideo= cv2.VideoWriter('./resultado.avi',codec,fps,tuple(it//2 for it in size))  
+# nuevoVideo= cv2.VideoWriter('./resultado.avi',codec,fps,size)  
 
-frame=video.read()
+ret, frame=video.read()
 rgb1= cv2.cvtColor(frame[::2,::2], cv2.COLOR_BGR2RGB)
-rgb2= cv2.cvtColor(frame[::2,::2], cv2.COLOR_BGR2RGB)
-cv2.imshow('1',rgb1)
-cv2.imshow('1',rgb2)
-
+# rgb2= cv2.cvtColor(frame[::2,::2], cv2.COLOR_BGR2RGB)
+# cv2.imshow('1',rgb1)
+# cv2.imshow('1',rgb2)
+cv2.imwrite("./res/test.jpg", rgb1)
